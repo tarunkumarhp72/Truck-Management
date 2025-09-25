eb49b3ea985b0550b76138d784adfa976f99c4d5
tarunkumar <tarunkumar@cssoftsolutions.com>
2025-09-25 12:32:13 +0530

    add cors

--- backend/truck_tracking/settings.py
+++ backend/truck_tracking/settings.py
@@ -157,6 +157,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://truck-management-iota.vercel.app/",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True  # Only for development
